bb61dd3c5c59ea07882156fb8273e1099fe3c7fc
unknown <arielc.nunes@outlook.com>
2024-02-26 10:36:49 +0000

    data binding lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListcomponentComponent } from './listcomponent/listcomponent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListcomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'data-binding-lab';
+  title:string = 'data-binding-lab';
+
+  // Increase count on click
+  timesClicked:number = 0;
+  onClick() {
+    this.timesClicked++;
+  }
+
+  // Hide h4 on double click
+  hidden:boolean = false;
+  imgDoubleClicked() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
